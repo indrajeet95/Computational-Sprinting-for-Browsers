81f83a4c3914e07f8d793d44629c146f970753da
Indrajeet <indrajeet95@gmail.com>
2018-05-30 15:47:07 -0400

    Util code for computing avg. load time and successful page loads from Results

---
+++ Util/average.py
@@ -0,0 +1,16 @@
+import os
+
+path = '/Users/indrajeet/Desktop/Results/30000/'
+for root,dirs,files in os.walk(path):
+	for file in files:
+		counter = 0
+		sum = 0
+		if file.endswith(".csv"):
+			f = open(path + '/' + file,'r')
+			for line in f:
+				if float(line.split(',')[-1]) <= 30000:
+					counter += 1
+					sum += float(line.split(',')[-1])
+			print '\n' + file + '\t'
+			print 'Successful Page loads: ' + str(counter)
+			print (sum + (100-counter)*30000)/100
